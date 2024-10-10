0bc75bd22edf61d3ee24dd84593ed634ae768cda
vibhor-eng <mayurty88@gmail.com>
2024-10-10 17:35:23 +0530

    update changes

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,60 @@
+let score = true
+
+//console.log(typeof score)
+///console.log(typeof(score))
+
+let valeInNumber = Number(score)
+
+//console.log(valeInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+//true => 1, false => 0
+
+let isLoggedIn = "ffdfdf";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+//1 => true 0 => false
+//"" => false;
+// "vibhor" => true
+
+let someNumber = 33;
+let someString = String(someNumber);
+// console.log(someString);
+// console.log(typeof(someString));
+
+
+
+//************************************Operations*******************************
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " Vibhor";
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("1" + 2); = 12
+
+//  console.log("1" + 2 + 2); = 122
+
+
+// console.log(1 + 2 + "2"); = 32
+
+// console.log(1 + "2" + 2); = 122
+
+// console.log(true); = true
+
+// console.log(+true); = 1
+
+//console.log(+"");=0
+
+//link to study
+//http://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
