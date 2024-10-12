1fe8561857d6228403f50b7aac24159bb317890e
vibhor-eng <mayurty88@gmail.com>
2024-10-12 18:33:31 +0530

    update basics

---
+++ 01_basics/06_numbers_maths.js
@@ -0,0 +1,23 @@
+// const score = 100
+// // console.log(score)
+
+// const balance = new Number(100)
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(1));
+
+
+
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(3));
+
+//+++++++++++Maths+++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+
+// console.log(Math.max(1,2,3,4))
---
+++ 01_basics/07_dates.js
@@ -0,0 +1,15 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate)
+
+let myCDate = new Date("01-14-2023")
+// console.log(myCDate.toDateString())
+
+// let myTimeStamp = Date.now();
+// console.log(myCDate.getTime())
+
+let newDate = new Date()
+console.log(newDate.getMonth() + 1)
+console.log(newDate.getDay())
