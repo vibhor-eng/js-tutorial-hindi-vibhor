528334157fbbe30df18d24706a59c8735eed1c3e
vibhor-eng <mayurty88@gmail.com>
2024-10-12 12:17:07 +0530

    update tutorial

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,14 @@
+// const name = "vibhor"
+// const count = 50
+
+// console.log(name + count);//old version
+
+// console.log(`Hello ${name} and my count is ${count}`)
+
+const gameName = new String("vibhor")
+
+console.log(gameName[0]);
+
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase())
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,32 @@
+const score = 100
+
+const scoreValue = 100.3
+
+const isLoggedIn = false
+
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const heros = [1,2,3]; //array
+
+const heroes = {
+
+    name:"vibhor",
+    score:5
+} //obk=ject
+
+const myFunction = function(){
+    console.log("sdfsd");
+}
+
+// console.log(typeof(myFunction));
+
+// Memory Types
+
+let channelName = "sddsd";
