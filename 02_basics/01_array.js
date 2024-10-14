a22a0ed0eb5298717aef439431338640e1df6ebc
vibhor-eng <mayurty88@gmail.com>
2024-10-14 13:33:09 +0530

    update function and array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,15 @@
+// const myArr = [1,2,3,4]
+// console.log(typeof(myArr))
+
+// myArr.push(5)
+
+// console.log(myArr);
+
+// myArr.pop()
+
+const A = [1,2,3];
+const B = [4,5,6];
+
+A.push(B);
+
+console.log(A);
