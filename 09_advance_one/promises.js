f3663e8f063bae3fc8aceabb42a33f78567ae395
vibhor-eng <mayurty88@gmail.com>
2024-10-24 14:00:07 +0530

    update

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -89,14 +89,14 @@ consumePromiseFive()
 
 //getAllUsers()
 
-// fetch('https://api.github.com/users/hiteshchoudhary')
-// .then((response) => {
-//     return response.json()
-// })
-// .then((data) => {
-//     console.log(data);
-// })
-// .catch((error) => console.log(error))
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
 
 // promise.all
 // yes this is also available, kuch reading aap b kro.
