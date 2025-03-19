8e9fc96a89d49f9148097c5b6b7198bd9da9cb90
vibhor-eng <mayurty88@gmail.com>
2025-03-19 10:51:55 +0530

    update date file

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -1,8 +1,8 @@
 let myDate = new Date()
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate)
+// console.log(myDate.toString()); //Wed Mar 19 2025 10:50:18 GMT+0530 (India Standard Time)
+// console.log(myDate.toDateString());//Wed Mar 19 2025
+// console.log(myDate.toLocaleString());//19/3/2025, 10:50:18 am
+// console.log(typeof myDate) //object
 
 let myCDate = new Date("01-14-2023")
 // console.log(myCDate.toDateString())
